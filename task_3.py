b5b377189032debec8856260fd64107026d9b62f
Marta <Martasunnyy@gmail.com>
2022-11-02 22:30:10 +0300

    homework3

---
+++ task_3.py
@@ -0,0 +1,16 @@
+# # Задайте список из вещественных чисел. Напишите программу,
+# которая найдёт разницу между максимальным и минимальным
+# значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+list = [round(random.uniform(1, 10), 2) for x in range(5)]
+print(list)
+list2 = [round((i%1),2) for i in list]
+min = 1
+max = 0
+for i in list2:
+    if i < min and i!=0: min = i      
+    if i > max: max = i
+print(round((max-min),2))
